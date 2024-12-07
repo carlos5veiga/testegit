fea7e07b08cd39dbedbc7508b40d8f145f0d1fc5
carlos5veiga <carlos5veiga@gmail.com>
2024-12-07 19:37:42 -0300

    seguindo

--- teste.js
+++ teste.js
@@ -1,1 +1,10 @@
-console.log("Testando");
+import http from "http";
+import fs from "fs";
+
+http.createServer(function (req, res) {
+    fs.readFile('teste.txt', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }).listen(3000);
