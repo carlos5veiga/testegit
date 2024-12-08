3a1c7ee4838cf62ebdb00606d8e6dd67d167c449
carlos5veiga <carlos5veiga@gmail.com>
2024-12-08 09:45:55 -0300

    mes

--- teste.js
+++ teste.js
@@ -6,23 +6,8 @@ const readableStream = fs.createReadStream("nivercorais.csv")
     .pipe(csv())
     .on('data', (data) => results.push(data))
     .on('end', () => {
-
-    const niverHoje = diaHoje();
-    const aniversariantes = [];
-    results.forEach((dado) => {
-        if (dado.Coral == "CMS" && dado.Niver == niverHoje) {       
-            aniversariantes.push(dado);
-        }
-    });
-
-    if (aniversariantes.length > 0) {       
-        console.log(`Hoje, ${niverHoje}, é aniversário de:`);
-        aniversariantes.forEach((pessoa) => {
-            console.log(`${pessoa.Nome}, ${pessoa.Naipe} - ${pessoa.Coral}`);
-        })
-    } else {
-        console.log(`Hoje, ${niverHoje}, não tem aniversário`);
-    }
+        aniversariantesDia();
+        aniversariantesMes();
   });
 
   function diaHoje() {
@@ -32,4 +17,54 @@ const readableStream = fs.createReadStream("nivercorais.csv")
     return dia + "/" + mes;
   }
 
+  function aniversariantesDia() {
+    console.log();
+    const niverHoje = diaHoje();
+    const aniversariantes = [];
+    results.forEach((dado) => {
+        if (dado.Niver == niverHoje) {       
+            aniversariantes.push(dado);
+        }
+    });
 
+    if (aniversariantes.length > 0) {
+        console.log(`Hoje, ${niverHoje}, é aniversário de:`);
+        aniversariantes.forEach((pessoa) => {
+            console.log(`${pessoa.Nome}, ${pessoa.Naipe} - ${pessoa.Coral}`);
+        })
+    } else {
+        console.log(`Hoje, ${niverHoje}, não tem aniversário`);
+    }
+  }
+
+  function aniversariantesMes() {
+    console.log();
+    const date = new Date();
+    const mes = ((date.getMonth()+1).toString().padStart(2, '0'));
+    const aniversariantes = [];
+    results.forEach((dado) => {
+        if (dado.Niver.slice(-2) == mes) {       
+            aniversariantes.push(dado);
+        }
+    });
+
+    const ordenado = ordenacaoData(aniversariantes);
+
+    if (aniversariantes.length > 0) {
+        console.log(`Aniversariantes do mês:`);
+        ordenado.forEach((pessoa) => {
+            console.log(`${pessoa.Niver} - ${pessoa.Nome}, ${pessoa.Naipe} - ${pessoa.Coral}`);
+        })
+    } else {
+        console.log(`Hoje, ${niverHoje}, não tem aniversário`);
+    }
+  }
+
+  function ordenacaoData(lista) {
+    const ordenado = lista.sort(sortfunction);
+    return ordenado;
+  }
+
+  function sortfunction(a, b){
+    return (a.Niver - b.Niver);
+  }
